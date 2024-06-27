e4758d29687c00c8e5234cba1485d16bae0ced3b
haydar kahla <haydarkahla@hotmail.com>
2024-06-27 07:28:57 +0200

    fix the configuration after deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,9 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' }, // Redirect empty path to '/profile'
+ 
+
+  { path: '', redirectTo: '/note', pathMatch: 'full' }, // Redirect empty path to '/profile'
 
   {
     path: '',
